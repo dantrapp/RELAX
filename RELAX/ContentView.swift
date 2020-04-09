a69139279adf1321c1fcd9a4262f1a9436e73be7
Dan Trapp <dtrapp@gmail.com>
2020-04-09 15:39:00 -0700

    Initial Commit

---
+++ RELAX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RELAX
+//
+//  Created by Will Hustle on 4/9/20.
+//  Copyright © 2020 Will Hustle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
